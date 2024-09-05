082c3265714124a7565f2547b4c557b4255115cf
Martyna Maciejewska <mmaciejewska@ironplane.com>
2024-09-05 12:06:19 +0200

    #106677 - Convert result to array if there is no payment methods

---
+++ src/Plugin/PaymentMethodsHelperPlugin.php
@@ -0,0 +1,19 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Snowdog\Hyva\Checkout\PayNow\Plugin;
+
+use Paynow\PaymentGateway\Helper\PaymentMethodsHelper;
+
+class PaymentMethodsHelperPlugin
+{
+    public function afterGetDigitalWalletsPaymentMethods(PaymentMethodsHelper $subject, ?array $result): array
+    {
+        if (is_null($result)) {
+            return [];
+        }
+
+        return $result;
+    }
+}
